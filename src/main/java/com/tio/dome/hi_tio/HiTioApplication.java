be86c59293300f761926d8d54fa4887aeebac49c
hejl1095 <1095735125@qq.com>
2023-02-19 23:32:10 +0800

    springBoot创建

---
+++ src/main/java/com/tio/dome/hi_tio/HiTioApplication.java
@@ -0,0 +1,13 @@
+package com.tio.dome.hi_tio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiTioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiTioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tio/dome/hi_tio/HiTioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tio.dome.hi_tio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiTioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
